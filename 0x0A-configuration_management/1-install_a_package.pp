adc87d65dc7fda9c757673ba8ffd6799864c1d26
root <root@Y4x>
2024-04-21 04:56:14 +0100

    fix 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
-# download flask
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Install puppet-lint
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
 
