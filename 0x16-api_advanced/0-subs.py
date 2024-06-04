d10ae54b174b9f91fa9bd25f78193819f3f3735e
root <root@Y4x>
2024-06-04 21:15:52 +0100

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,7 +2,9 @@
 """
 Script that queries subscribers on a given Reddit subreddit.
 """
+
 import requests
+
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
