a1f2af97a67698ebfce0d135b39d3c14701b04bd
root <root@Y4x>
2024-06-09 03:50:51 +0100

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
-# fixes a file
-exec {'replaces wrong php filetype':
-  command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
-  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
-  onlyif  => 'test -f /var/www/html/wp-settings.php'
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
 
