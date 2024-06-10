cd9f7b20cce6e121e0d6904d82f65d6a057db60e
root <root@Y4x>
2024-06-10 03:09:06 +0100

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,13 @@
-# fixes a file
-exec { 'fix the php extension issue':
+#Fixing a Permissions Issue
+
+exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
+  onlyif  => 'grep -q phpp /var/www/html/wp-settings.php',
+  notify  => Service['apache2'],
+}
+
+service { 'apache2':
+  ensure => running,
+  enable => true,
 }
