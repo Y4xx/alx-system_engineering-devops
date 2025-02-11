8d61e2df3ec37a4b2fca2b5612750bbdff94a1ba
Yassine OUJAMA <yassine.oujama@gmail.com>
2025-02-11 15:18:23 +0100

    fix

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+this code will get data from api and process it
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    """
+    main function
+    """
+    argc = len(sys.argv)
+    user_id = sys.argv[1]
+    tasks_completed = 0
+    number_of_tsks = 0
+
+    api_url_users = 'https://jsonplaceholder.typicode.com/users'
+    api_url_todos = 'https://jsonplaceholder.typicode.com/todos'
+
+    result_users = requests.get(api_url_users + '/' + user_id)
+    result_todos = requests.get(api_url_todos)
+
+    result_users = json.loads(result_users.text)
+    result_todos = json.loads(result_todos.text)
+
+    user_todos = []
+    for todo in result_todos:
+        if todo['userId'] == int(user_id):
+            number_of_tsks += 1
+            user_todos.append(todo)
+            if todo['completed'] is True:
+                tasks_completed += 1
+
+    print(f"Employee {result_users['name']} is done with", end='')
+    print(f" tasks({tasks_completed}/{number_of_tsks}):")
+    for todo in user_todos:
+        if todo['completed'] is True:
+            print(f"\t {todo['title']}")
